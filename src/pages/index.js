2ea47ea198d9fa7212ca921f1600628aee48b97b
maki005 <einsfam.m@gmail.com>
2020-06-10 15:33:28 +0900

    Add:DOM要素追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
+import './styles/style.scss'
+import { graphql } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <section id="eyecatch">
+        <h1>Hello Gatsby!</h1>
+      </section>
+
+      <section className="wrap">
+        <h2>Works</h2>
+          <ul>
+            <li>自社採用サイト</li>
+            <li>商品検索システム</li>
+            <li>ナレッジ検索システム</li>
+            <li>契約管理システム</li>
+          </ul>
+      </section>
+
+      <section className="wrap">
+        <h2>Profile</h2>
+      </section>
+
+      <section className="wrap">
+        日報・Twitter
+      </section>
+
+      <img src="/images/news.png" alt="アイコン" />
+    </div>
+  )
 }
