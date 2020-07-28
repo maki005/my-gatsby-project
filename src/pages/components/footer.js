7de97984e874904d61fe1f7ab648cdd7923fbb5a
maki005 <einsfam.m@gmail.com>
2020-07-28 16:14:01 +0900

    Style:footer見た目調整

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -3,8 +3,9 @@ import styles from '../styles/footer.module.css'
 
 export default function Footer() {
     return (
-        <footer className={styles.container}>
-            <div>MAKI ADACHI Portfolio</div>
+        <footer className={styles.footer}>
+            <div className={styles.copyright}>MAKI ADACHI Portfolio | 2020</div>
+            <span className={styles.container}></span>
         </footer>
     )
 }
