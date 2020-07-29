f1732562c2b7d9a505b476e8f4f50439ff0ae8a6
maki005 <einsfam.m@gmail.com>
2020-07-29 10:50:36 +0900

    Style:プロフィールページの見た目調整

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -9,7 +9,33 @@ export default function Profile({ date }) {
         <div>
             <Layout>
                 <h1 className={styles.eyecatch}>Profile</h1>
-                <div>スライドとか入れる</div>
+                <div className="wrap wrap_profile">
+                    <p><span className="bold f_large mR_1">足立&nbsp;&nbsp;真規</span>1994年生まれ&nbsp;関西在住</p>
+                    <p>
+                        高知大学人文学部国際社会コミュニケーション学科卒業後、未経験でキー・ポイント株式会社へ入社。
+                        自社プロダクト（BtoB系SaaS）のUI作成・改善や紹介HPの作成、自社サイトの運営に携わりながら、受託での単発開発システムのUIも担当するなど。
+                        デザインだけでなく、コーディングやPJによってはディレクションなどをすることも。好きなことはバスケと絵を描くこと。
+                    </p>
+                    <div className={styles.wrap_list_point}>
+                        <div>
+                            <h3 className="bold f_middle mB_1">得意なこと</h3>
+                            <ul className={styles.list_point}>
+                                <li>BtoB系システムのUI作成</li>
+                                <li>自社サイト運営・コンテンツ制作</li>
+                                <li>プロトタイプ作成</li>
+                                <li>コーディング（HTML/CSS）</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 className="bold f_middle mB_1">苦手・勉強中のもの</h3>
+                            <ul className={styles.list_point}>
+                                <li>Google Analyticsのデータからの検証</li>
+                                <li>グラフィック</li>
+                                <li>JavaScript</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
 
                 <div className={styles.wrap_ex}>
