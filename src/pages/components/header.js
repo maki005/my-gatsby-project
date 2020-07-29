2eccf52ed3641c33da0f8c510d8f85fcb0519895
maki005 <einsfam.m@gmail.com>
2020-07-29 10:51:59 +0900

    Style:header見た目修正

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,13 +1,16 @@
 import React from "react"
 import styles  from  '../styles/header.module.css'
+import { Link } from "gatsby"
+
+
 
 export default function Header() {
     return (
         <header>
             <div className={styles.container}>
                 <ul className={styles.list}>
-                    <li>TOP</li>
-                    <li>Profile</li>
+                    <Link to={`/`}><li>TOP</li></Link>
+                    <Link to={`/profile/`}><li>Profile</li></Link>
                 </ul>
             </div>
         </header>
