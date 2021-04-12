833133db927aed4acb6f0be8f409977b9b80373b
maki005 <einsfam.m@gmail.com>
2021-04-12 22:10:00 +0900

    change:topのいらない部分削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,50 +32,6 @@ export default function Home({ data }) {
       </div>
       </section>
 
-      <section className="wrap">
-        <h2 className="sub_title">できること</h2>
-        <div className="d_flex">
-          <div className="card ui">
-            <h3>UIデザイン</h3>
-            <p>自社製品のデザインを担当しています。Adobe XDでのモックからHTML/CSSまでを担当</p>
-          </div>
-          <div className="card web">
-            <h3>Webデザイン</h3>
-            <p>自社サイト及び製品紹介サイトをWordPressで管理。LPや自社採用ページなども作成</p>
-          </div>
-          <div className="card engineer">
-            <h3>エンジニアと</h3>
-            <p>エンジニアとプロジェクトを進める。ローカル環境構築やGithubでのソース管理なども業務内での経験。</p>
-          </div>
-        </div>
-      </section>
-
-      <section className="wrap wrap_profile">
-        <h2 className="sub_title">プロフィール</h2>
-        <div>
-          <Link to={`/profile/`}><span className="link_profile">足立 真規</span></Link>
-          <span>1994年生まれ　関西在住</span>
-        </div>
-        <p>
-          高知大学人文学部卒業。転職活動をするうちにこれからの動向を検討し、IT業界への就職を決め
-          未経験で現在の会社に新卒入社。Web UIデザイナーとして自社システムのUIを担当しています。
-          エンジニアと共同で働きながらプログラムやネットワーク関連についても学びながら働いています。
-          
-          <br />
-          知らないことを知れる、できないことができるということに喜びを感じる人間です。
-        </p>
-        <div className="wrap_tool">
-          <span className="tool">Adobe XD</span>
-          <span className="tool">Photoshop</span>
-          <span className="tool">illustrater</span>
-          <span className="tool">HTML</span>
-          <span className="tool">CSS</span>
-          <span className="tool">JavaScript</span>
-          <span className="tool">GitHub</span>
-          <span className="tool">WordPress</span>
-        </div>
-      </section>
-
     </Layout>
 
   )
