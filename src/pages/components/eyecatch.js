2d1b1d9d71cee719d70507f1c80fe94059102bf0
maki005 <einsfam.m@gmail.com>
2021-08-31 21:34:39 +0900

    change:eyecatchデザイン変更

--- src/pages/components/eyecatch.js
+++ src/pages/components/eyecatch.js
@@ -8,8 +8,8 @@ export default function Eyecatch(){
                 <div className={styles.eyecatch}>
                     <div className={styles.inner_eyecatch}>
                         <div className={styles.wrap_title}>
-                            <h1 className={styles.title}>PORTFOLIO</h1>
-                            <div>Web UI/UX Designer</div>
+                            <h1 className={styles.title}>PO<span className={styles.color}>R</span>TFOLIO</h1>
+                            <div className={styles.sub_title}>Web UI/UX Designer</div>
                         </div>
                     </div>
                 </div>
